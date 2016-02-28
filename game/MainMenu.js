28688529566ebfc517936115d482db6e4e715bab
WesleyTsai <tsai.wesley@gmail.com>
2016-02-27 20:44:20 -0800

    Changed pacman references to tron

--- game/MainMenu.js
+++ game/MainMenu.js
@@ -1,20 +1,23 @@
-Pacman = {
-    score: 0,
+Tron = {
+    score: 0
 };
 
-Pacman.MainMenu = function(game) {
+Tron.MainMenu = function(game) {
 };
 
-Pacman.MainMenu.prototype = {
+Tron.MainMenu.prototype = {
     preload: function() {
         this.load.image('menu_background', 'images/mainmenu.png');
+        this.load.image('start_button', 'images/startbutton.png');
         this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
     },
 
     create: function() {
         this.stage.setBackgroundColor(0x2d2d2d);
         this.add.sprite(0, 0, 'menu_background');
-        this.add.bitmapText(0, 64, 'webfont', 'PACMAN', 32);
+
+        var button = this.add.button(this.world.centerX - 80, 400, 'start_button', this.startGame, this);
+        button.anchor.setTo(0.5,0.5);
     },
 
     update: function() {
