d3dc7dedca9536e091a99d2c07f1db799e53f942
Angela <angela.hanna.cho@gmail.com>
2016-02-28 03:27:11 -0800

    tron1 farts

--- game/game.js
+++ game/game.js
@@ -13,6 +13,7 @@ var upKey;
 var downKey;
 var leftKey;
 var rightKey;
+var emitter;
 
 Hackatron.Game.prototype = {
     preload: function() {
@@ -20,6 +21,7 @@ Hackatron.Game.prototype = {
         this.load.image('tiles', 'assets/part2_tileset.png');
         this.load.spritesheet('tron', 'images/tron.png', 32, 32, 12);
         this.load.spritesheet('ghost', 'images/ghost.png', 32, 32, 12);
+		this.load.image('blueball', 'images/blueball.png');
     },
 
     create: function() {
@@ -51,6 +53,18 @@ Hackatron.Game.prototype = {
     	ghost1.leftKey = this.input.keyboard.addKey(Phaser.Keyboard.A);
     	ghost1.rightKey = this.input.keyboard.addKey(Phaser.Keyboard.D);
 
+		emitter = this.add.emitter(tron1.x, tron1.y, 50);
+		emitter.width = 5;
+		emitter.makeParticles('blueball');
+		emitter.setXSpeed();
+		emitter.setYSpeed();
+		emitter.setRotation();
+		emitter.setAlpha(1, 0.01, 800);
+		emitter.setScale(0.05, 0.4, 0.05, 0.4, 2000, Phaser.Easing.Quintic.Out);
+		emitter.start(false,500, 2);
+		
+		
+		
         // Add score text
         this.scoreText = this.add.text(this.world.width - 128, 0, 'Score: 0');
         this.scoreText.addColor('White', 0);
@@ -65,21 +79,30 @@ Hackatron.Game.prototype = {
         if (character.upKey.isDown) {
             character.animations.play('walkUp', 3, false);
             character.y -= speed;
+			emitter.x = tron1.x + 15;
+			emitter.y = tron1.y + 30;
         } else if (character.downKey.isDown) {
             character.animations.play('walkDown', 3, false);
             character.y += speed;
+			emitter.x = tron1.x + 15;
+			emitter.y = tron1.y;
+
         } else if (character.leftKey.isDown) {
             character.animations.play('walkLeft', 3, false);
             character.x -= speed;
             if (character.x < 0) {
                 character.x = this.world.width;
             }
+			emitter.x = tron1.x + 50;
+			emitter.y = tron1.y + 15;
         } else if (character.rightKey.isDown) {
             character.animations.play('walkRight', 3, false);
             character.x += speed;
             if (character.x > this.world.width) {
                 character.x = 0;
             }
+			emitter.x = tron1.x;
+			emitter.y = tron1.y + 15;
         }
 
         return {x: character.x, y: character.y};
