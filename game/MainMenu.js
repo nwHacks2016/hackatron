c378b615968f25f7efa6dbee025dd442e9bbde2d
WesleyTsai <tsai.wesley@gmail.com>
2016-02-27 20:13:22 -0800

    Added a background to the main menu

--- game/MainMenu.js
+++ game/MainMenu.js
@@ -1,10 +1,26 @@
+Pacman = {
+    score: 0,
+};
+
 Pacman.MainMenu = function(game) {
-    
 };
 
 Pacman.MainMenu.prototype = {
-    create: function() {
-        this.add.image(0, 0, 'background');
+    preload: function() {
+        this.load.image('menu_background', 'images/background.png');
+        this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
+    },
 
+    create: function() {
+        this.stage.setBackgroundColor(0x2d2d2d);
+        this.add.sprite(0, 0, 'menu_background');
+        this.add.bitmapText(0, 64, 'webfont', 'PACMAN', 32);
+    },
+
+    update: function() {
+    },
+
+    startGame: function(){
+        this.state.start('Game');
     }
 }
