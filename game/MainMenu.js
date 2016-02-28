72ac2fc314f482114e4a77cc33cf5f5234384845
WesleyTsai <tsai.wesley@gmail.com>
2016-02-27 20:24:40 -0800

    changed background

--- game/MainMenu.js
+++ game/MainMenu.js
@@ -7,7 +7,7 @@ Pacman.MainMenu = function(game) {
 
 Pacman.MainMenu.prototype = {
     preload: function() {
-        this.load.image('menu_background', 'images/background.png');
+        this.load.image('menu_background', 'images/mainmenu.png');
         this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
     },
 
