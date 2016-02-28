5715e7868127f30ffb41237302b6e7c5d4b54756
stellafang <stellafangg@gmail.com>
2016-02-28 00:11:13 -0800

    added keys and sprites

--- game/game.js
+++ game/game.js
@@ -5,10 +5,22 @@ Hackatron = {
 Hackatron.Game = function(game) {
 };
 
+var tron1;
+var ghost1;
+
+var upKey;
+var downKey;
+var leftKey;
+var rightKey;
+
 Hackatron.Game.prototype = {
     preload: function() {
         this.load.tilemap('map', 'assets/tiles1.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('tiles', 'assets/plums.png');
+
+        this.load.image('tron', 'assets/bluesprite.png');
+        this.load.image('ghost', 'assets/yellowsprite.png');
+
     },
 
     create: function() {
@@ -17,9 +29,41 @@ Hackatron.Game.prototype = {
 
         this.layer = this.map.createLayer('Tile Layer 1');
         this.layer.resizeWorld();
+
+        tron1 = this.add.sprite(50, 50, 'tron');
+        tron1.scale.x = 0.2;
+        tron1.scale.y = 0.2;
+        ghost1 = this.add.sprite(70, 70, 'ghost');
+        ghost1.scale.x = 0.2;
+        ghost1.scale.y = 0.2;
+
+        upKey = this.input.keyboard.addKey(Phaser.Keyboard.UP);
+    	downKey = this.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    	leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    	rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+
     }, 
 
     update: function() {
+
+    	if (upKey.isDown) {
+    		console.log('hi');
+    		//sprite.y--;
+    	}
+    	else if (downKey.isDown)
+    	{
+        	//sprite.y++;
+    	}
+
+    	if (leftKey.isDown)
+    	{
+        	//sprite.x--;
+    	}
+    	else if (rightKey.isDown)
+    	{
+        	//sprite.x++;
+    	}
     }
 };
 
