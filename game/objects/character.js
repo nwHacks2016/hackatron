0dccac4134ac32604f88e28c9aeee426a5c2b767
Tony Dinh <tdinh@mobify.me>
2016-02-27 18:56:30 -0800

    add primitive object character.js

---
+++ game/objects/character.js
@@ -0,0 +1,39 @@
+define([], function() {
+	var Character = function(params) {
+		this.position = params.pos;
+	};
+
+	Character.init = function(name) {
+		var params = {
+			pos: {
+				x: 0,
+				y: 0
+			},
+			name: name
+		};
+
+		return new Character(params);
+	};
+
+	Character.protoype.up = function() {
+		this.position.y = this.y + 1;
+	};
+
+	Character.protoype.down = function() {
+		this.position.y = this.y - 1;
+	};
+
+	Character.protoype.left = function() {
+		this.position.x = this.x - 1;
+	};
+
+	Character.protoype.right = function() {
+		this.position.x = this.x + 1;
+	};
+
+	Character.protoype.getPosition = function() {
+		return this.position;
+	};
+
+	return Character;
+})
