d841abad6dce802ecefdf231c92a977a42207a1b
tony-dinh <tonyd@sfu.ca>
2016-03-13 14:37:29 -0700

    Countdown should look agressive

--- src/objects/countdown.js
+++ src/objects/countdown.js
@@ -17,7 +17,7 @@ Countdown.prototype.start = function() {
     var count3 = self.game.add.sprite(centerX, centerY, 'countdown');
     count3.frame = 2;
     count3.anchor.setTo(0.5);
-    tween3 = self.game.add.tween(count3.scale).to({ x: 3, y: 3}, 500, "Linear", true);
+    tween3 = self.game.add.tween(count3.scale).to({ x: 3, y: 3}, 600, Phaser.Easing.Exponential.In, true);
 
     tween3.onComplete.add(function() {
         self.game.add.tween(count3).to( { alpha: 0 }, 200, "Linear", true);
@@ -27,7 +27,7 @@ Countdown.prototype.start = function() {
         count2.frame = 1;
         count2.anchor.setTo(0.5);
 
-        tween2 = self.game.add.tween(count2.scale).to({ x: 3, y: 3}, 500, "Linear", true);
+        tween2 = self.game.add.tween(count2.scale).to({ x: 3, y: 3}, 600, Phaser.Easing.Exponential.In, true);
         tween2.onComplete.add(function() {
             self.game.add.tween(count2).to( { alpha: 0 }, 200, "Linear", true);
 
@@ -36,7 +36,7 @@ Countdown.prototype.start = function() {
             count1.frame = 0;
             count1.anchor.setTo(0.5);
 
-            tween1 = self.game.add.tween(count1.scale).to({ x: 3, y: 3}, 500, "Linear", true);
+            tween1 = self.game.add.tween(count1.scale).to({ x: 3, y: 3}, 600, Phaser.Easing.Exponential.In, true);
             tween1.onComplete.add(function() {
                 self.game.add.tween(count1).to( { alpha: 0 }, 200, "Linear", true);
             });
