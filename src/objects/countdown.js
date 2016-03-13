1dfd26f7a3c41cff8e9198ba629dc3e4e2157d0f
Tony Dinh <tdinh@mobify.me>
2016-03-13 05:08:45 -0700

    countdown should COUNT DOWN

--- src/objects/countdown.js
+++ src/objects/countdown.js
@@ -13,33 +13,32 @@ Countdown.prototype.start = function() {
     var centerX = self.game.camera.width / 2;
     var centerY = self.game.camera.height / 2;
 
-    var count1 = self.game.add.sprite(centerX, centerY, 'countdown');
-    count1.frame = 0;
-    count1.anchor.setTo(0.5);
-    tween1 = self.game.add.tween(count1.scale).to({ x: 3, y: 3}, 500, "Linear", true);
+    // Countdown #3
+    var count3 = self.game.add.sprite(centerX, centerY, 'countdown');
+    count3.frame = 2;
+    count3.anchor.setTo(0.5);
+    tween3 = self.game.add.tween(count3.scale).to({ x: 3, y: 3}, 500, "Linear", true);
 
-    tween1.onComplete.add(function() {
-        // tween1.stop();
-        self.game.add.tween(count1).to( { alpha: 0 }, 200, "Linear", true);
+    tween3.onComplete.add(function() {
+        self.game.add.tween(count3).to( { alpha: 0 }, 200, "Linear", true);
 
+        // Countdown #2
         var count2 = self.game.add.sprite(centerX, centerY, 'countdown');
         count2.frame = 1;
         count2.anchor.setTo(0.5);
 
         tween2 = self.game.add.tween(count2.scale).to({ x: 3, y: 3}, 500, "Linear", true);
         tween2.onComplete.add(function() {
-            // tween2.stop();
             self.game.add.tween(count2).to( { alpha: 0 }, 200, "Linear", true);
 
+            // Countdown #1
+            var count1 = self.game.add.sprite(centerX, centerY, 'countdown');
+            count1.frame = 0;
+            count1.anchor.setTo(0.5);
 
-            var count3 = self.game.add.sprite(centerX, centerY, 'countdown');
-            count3.frame = 2;
-            count3.anchor.setTo(0.5);
-
-            tween3 = self.game.add.tween(count3.scale).to({ x: 3, y: 3}, 500, "Linear", true);
-            tween3.onComplete.add(function() {
-                self.game.add.tween(count3).to( { alpha: 0 }, 200, "Linear", true);
-
+            tween1 = self.game.add.tween(count1.scale).to({ x: 3, y: 3}, 500, "Linear", true);
+            tween1.onComplete.add(function() {
+                self.game.add.tween(count1).to( { alpha: 0 }, 200, "Linear", true);
             });
         });
     });
