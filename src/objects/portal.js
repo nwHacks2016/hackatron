3729317b927a29e22505cf459e3d40b964d7dd6e
Eric Muyser <eric@muyser.com>
2016-03-13 00:09:50 -0800

    Clean up portal variables

--- src/objects/portal.js
+++ src/objects/portal.js
@@ -8,18 +8,18 @@ Portal.prototype.constructor = Portal;
 Portal.prototype.init = function(game, data) {
     this.data = data;
     console.log(data);
-    this.bIsEntryGood = false;
-    this.bIsExitGood = false
+    this.isEntryGood = false;
+    this.isExitGood = false
     var entryPortalCoord;
     var exitPortalCoord;
-    while (!this.bIsExitGood || !this.bIsEntryGood) {
-        if (!this.bIsEntryGood) {
-            entryPortalCoord = this.checkIfVaildCoord(game.rnd.integerInRange(0, 32), game.rnd.integerInRange(0, 32));
-            this.bIsEntryGood = entryPortalCoord.clean;
+    while (!this.isExitGood || !this.isEntryGood) {
+        if (!this.isEntryGood) {
+            entryPortalCoord = this.checkIfValidCoord(game.rnd.integerInRange(0, 32), game.rnd.integerInRange(0, 32));
+            this.isEntryGood = entryPortalCoord.clean;
         }
-        if (!this.bIsExitGood) {
-            exitPortalCoord = this.checkIfVaildCoord(game.rnd.integerInRange(0, 32), game.rnd.integerInRange(0, 32));
-            this.bIsExitGood = exitPortalCoord.clean;
+        if (!this.isExitGood) {
+            exitPortalCoord = this.checkIfValidCoord(game.rnd.integerInRange(0, 32), game.rnd.integerInRange(0, 32));
+            this.isExitGood = exitPortalCoord.clean;
         }
 
     }
@@ -39,15 +39,15 @@ Portal.prototype.init = function(game, data) {
 Portal.prototype.update = function() {
 };
 
-Portal.prototype.checkIfVaildCoord = function(x, y) {
+Portal.prototype.checkIfValidCoord = function(x, y) {
     // data goes top to down and left to right
     var value = this.data[y * 32 + x];
     console.log(value);
-    var bClean = true;
+    var clean = true;
     if (value !== 0)
-        bClean = false;
+        clean = false;
     return {
-        clean: bClean,
+        clean: clean,
         x: x / 32 * 32 + 20, // floor it and make it the mutiple of 32
         y: y * 32 % 470
     }
