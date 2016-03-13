5fb80484668fc628aecc16e5b0448916f7fbece5
Tony Dinh <tdinh@mobify.me>
2016-03-13 04:58:19 -0700

    countdown should be anchored to center

--- src/objects/countdown.js
+++ src/objects/countdown.js
@@ -15,6 +15,7 @@ Countdown.prototype.start = function() {
 
     var count1 = self.game.add.sprite(centerX, centerY, 'countdown');
     count1.frame = 0;
+    count1.anchor.setTo(0.5);
     tween1 = self.game.add.tween(count1.scale).to({ x: 3, y: 3}, 500, "Linear", true);
 
     tween1.onComplete.add(function() {
@@ -23,6 +24,8 @@ Countdown.prototype.start = function() {
 
         var count2 = self.game.add.sprite(centerX, centerY, 'countdown');
         count2.frame = 1;
+        count2.anchor.setTo(0.5);
+
         tween2 = self.game.add.tween(count2.scale).to({ x: 3, y: 3}, 500, "Linear", true);
         tween2.onComplete.add(function() {
             // tween2.stop();
@@ -31,6 +34,8 @@ Countdown.prototype.start = function() {
 
             var count3 = self.game.add.sprite(centerX, centerY, 'countdown');
             count3.frame = 2;
+            count3.anchor.setTo(0.5);
+
             tween3 = self.game.add.tween(count3.scale).to({ x: 3, y: 3}, 500, "Linear", true);
             tween3.onComplete.add(function() {
                 self.game.add.tween(count3).to( { alpha: 0 }, 200, "Linear", true);
